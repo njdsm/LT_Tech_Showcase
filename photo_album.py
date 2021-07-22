06076c1c17da652d38dfb158a1a43af5b387f2fd
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-07-22 13:09:56 -0500

    after looking at the document again I realized there was no need for user interaction.

--- photo_album.py
+++ photo_album.py
@@ -1,21 +1,15 @@
-from photos import Photos
 import urllib.request, json
 
 
 class PhotoAlbum:
 
-    def __init__(self):
-        pass
-
     def run(self):
         with urllib.request.urlopen("https://jsonplaceholder.typicode.com/photos") as url:
             data = json.loads(url.read().decode())
-            will_proceed = True
-            while will_proceed:
-                user_option = input("Type the album number you would like to view contents of: ")
-                output_dict = [x for x in data if x['albumId'] == int(user_option)]
-                print(" > photo-album " + str(user_option))
-                for x in output_dict:
-                    print("[" + str(x.get('id')) + "]" + " " + x.get('title'))
-                will_proceed = False
-
+            print(" > photo-album " + "1")
+            current_album = 1
+            for x in data:
+                if current_album != x.get('albumId'):
+                    print("\n > photo-album " + str(x.get('albumId')) + "\n")
+                current_album = x.get('albumId')
+                print("[" + str(x.get('id')) + "]" + " " + x.get('title'))
